b517557781639c11931fc9c2620f0fab6130d014
Aleksey Zemskov <zemskov.aleksey@gmail.com>
2020-09-17 17:24:47 +0300

    ...

---
+++ new_cert.ps1
@@ -0,0 +1,40 @@
+# setup certificate properties including the commonName (DNSName) property for Chrome 58+
+$certificate = New-SelfSignedCertificate `
+    -Subject onelocalhost `
+    -DnsName onelocalhost `
+    -KeyAlgorithm RSA `
+    -KeyLength 2048 `
+    -NotBefore (Get-Date) `
+    -NotAfter (Get-Date).AddYears(2) `
+    -CertStoreLocation "cert:CurrentUser\My" `
+    -FriendlyName "onelocalhost Certificate for .NET Core" `
+    -HashAlgorithm SHA256 `
+    -KeyUsage DigitalSignature, KeyEncipherment, DataEncipherment `
+    -TextExtension @("2.5.29.19={critical}{text}","2.5.29.37={critical}{text}1.3.6.1.5.5.7.3.1","2.5.29.17={critical}{text}DNS=onelocalhost")
+$certificatePath = 'Cert:\CurrentUser\My\' + ($certificate.ThumbPrint)  
+
+# create temporary certificate path
+$tmpPath = "C:\tmp"
+If(!(test-path $tmpPath))
+{
+New-Item -ItemType Directory -Force -Path $tmpPath
+}
+
+# set certificate password here
+$pfxPassword = ConvertTo-SecureString -String "12345" -Force -AsPlainText
+$pfxFilePath = "c:\tmp\onelocalhost.pfx"
+$cerFilePath = "c:\tmp\onelocalhost.cer"
+
+# create pfx certificate
+Export-PfxCertificate -Cert $certificatePath -FilePath $pfxFilePath -Password $pfxPassword
+Export-Certificate -Cert $certificatePath -FilePath $cerFilePath
+
+# import the pfx certificate
+Import-PfxCertificate -FilePath $pfxFilePath Cert:\LocalMachine\My -Password $pfxPassword -Exportable
+
+# trust the certificate by importing the pfx certificate into your trusted root
+Import-Certificate -FilePath $cerFilePath -CertStoreLocation Cert:\CurrentUser\Root
+
+# optionally delete the physical certificates (don’t delete the pfx file as you need to copy this to your app directory)
+# Remove-Item $pfxFilePath
+Remove-Item $cerFilePath
